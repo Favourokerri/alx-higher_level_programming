a02c52f55376bccdc781b8215d892002aa131391
Favour okerri <favourokerri767@gmail.com>
2023-05-09 10:54:47 -0700

    100-print_tebahpla.py

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,4 +1,7 @@
 #!/usr/bin/python3
-
 for i in range(ord('z'), ord('a') - 1, -1):
-    print('{:c}'.format(i - 32 if (i - ord('z')) % 2 == 0 else i), end='')
+    if i % 2 == 0:
+        even = 0
+    else:
+        even = 32
+    print('{}'.format(chr(i - even)), end='')
