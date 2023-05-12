2a8aae0dc491d6077452c02bac7c6ef6d638ad66
Favour okerri <favourokerri767@gmail.com>
2023-05-12 12:58:50 -0700

    7-add_tuple.py

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -13,3 +13,25 @@ def add_tuple(tuple_a=(), tuple_b=()):
         add1 = int(tuple_a[0]) + int(tuple_b[0])
         add2 = int(tuple_a[1]) + int(new_b[1])
         return ((add1, add2))
+    elif len_tuple_a == 1:
+        new_element = 0
+        new_a = tuple_a + (new_element,)
+        add1 = int(tuple_a[0]) + int(tuple_b[0])
+        add2 = int(new_a[1]) + int(tuple_b[1])
+        return ((add1, add2))
+    elif len_tuple_a == 0 and len_tuple_b != 0:
+        new_a = tuple_a + (0, 0)
+        add1 = int(new_a[0]) + int(tuple_b[0])
+        add2 = int(new_a[1]) + int(tuple_b[1])
+        return ((add1, add2))
+    elif len_tuple_b == 0 and len_tuple_a != 0:
+        new_b = tuple_b + (0, 0)
+        add1 = int(tuple_a[0]) + int(new_b[0])
+        add2 = int(tuple_a[1]) + int(new_b[1])
+        return ((add1, add2))
+    else:
+        new_a = tuple_a + (0, 0)
+        new_b = tuple_b + (0, 0)
+        add1 = int(new_a[0]) + int(new_b[0])
+        add2 = int(new_a[1]) + int(new_b[1])
+        return ((add1, add2))
