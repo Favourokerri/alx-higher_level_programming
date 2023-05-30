3ddf2ef7e9d110c1c5b02816792f5fe3e74110e6
Favour okerri <favourokerri767@gmail.com>
2023-05-30 00:39:37 -0700

    2-square.py

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+class Square:
+    """class Square that defines a square by"""
+    def __init__(self, size):
+        """initializing the square class
+        Args:
+            size: represents the size of square
+        """
+        self.__size = size
