e85fb95ce58351ee5ed3b89bcbcd6e1d0f7b4b7b
Favour okerri <favourokerri767@gmail.com>
2023-06-06 12:29:14 -0700

    19-main.py

--- 0x09-python-everything_is_object/19-copy_list.py
+++ 0x09-python-everything_is_object/19-copy_list.py
@@ -1,12 +1,3 @@
 #!/usr/bin/python3
-"""returns a copy of a string """
-
-
-def copy_list(my_list):
-    """ returns copy if a list
-    Args:
-        my_list - list to be copied
-    """
-    l_2 = []
-    l_2 = l_2 + my_list
-    return (l_2)
+def copy_list(mylist):
+    return (mylist.copy())
