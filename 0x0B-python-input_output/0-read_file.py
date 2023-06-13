8e8b84ec97c8ee39e90a31e0631038a23a229238
Favour okerri <favourokerri767@gmail.com>
2023-06-13 13:52:31 -0700

    0-read_file.py

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -4,6 +4,6 @@
 
 def read_file(filename=""):
     """function that reads a file in utf8 encoding format """
-    with open("filename", encoding="UTF8") as f:
+    with open(filename, encoding="UTF8") as f:
         read_data = f.read()
-    return ("read_data")
+    return (read_data)
