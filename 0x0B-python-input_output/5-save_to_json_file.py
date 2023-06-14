624bc09acad3ca7ee377ecb92991d2e8c35d1821
Favour okerri <favourokerri767@gmail.com>
2023-06-13 18:13:49 -0700

    5-save_to_json_file.py

---
+++ 0x0B-python-input_output/5-save_to_json_file.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""Write a function that writes an Object to a text file, using a JSON representation"""
+import json
+
+
+def save_to_json_file(my_obj, filename):
+    """function that writes an object to a file usuing json
+    Args:
+        my_obj - the object
+        filename - name of the file
+    """
+    with open("filename", "w", encoding="UTF8") as f:
+        json.dump(my_obj, f)
