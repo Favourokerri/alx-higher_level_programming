3e666fb8396b21ad9854a179ec2295d96261fb45
Favourokerri <favourokerri767@gmail.com>
2023-06-17 20:53:39 +0000

    test

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -35,7 +35,7 @@ class Rectangle(Base):
     @width.setter
     def width(self, value):
         """setter for width """
-        if not isinstance(value, int):
+        if (type(value) is not int):
             raise TypeError("width must be an integer")
         if value <= 0:
             raise ValueError("width must be > 0")
@@ -45,7 +45,7 @@ class Rectangle(Base):
     @height.setter
     def height(self, value):
         """setter for height """
-        if not isinstance(value, int):
+        if (type(value) is not int):
             raise TypeError("height must be an integer")
         if value <= 0:
             raise ValueError("height must be > 0")
@@ -54,7 +54,7 @@ class Rectangle(Base):
     @x.setter
     def x(self, value):
         """setter for x """
-        if not isinstance(value, int):
+        if (type(value) is not int):
             raise TypeError("x must be an integer")
         if value < 0:
             raise ValueError("x must be >= 0")
@@ -63,7 +63,7 @@ class Rectangle(Base):
     @y.setter
     def y(self, value):
         """setter for y """
-        if not isinstance(value, int):
+        if (type(value) is not int):
             raise TypeError("y must be an integer ")
         if value < 0:
             raise ValueError("y must be >= 0")
@@ -81,4 +81,5 @@ class Rectangle(Base):
 
     def __str__(self):
         """prints out the object """
-        return f"[Rectangle] ({self.id}) {self.__x}/{self.__y} - {self.__width}/{self.__height}"
+        return f"[Rectangle] ({self.id}) {self.__x}/{self.__y} - \
+{self.__width}/{self.__height}"
