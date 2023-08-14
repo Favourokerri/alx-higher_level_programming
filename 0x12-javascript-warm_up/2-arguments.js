18ba5bfc603199e34a8ddd6863b850681f25bb35
Favourokerri <favourokerri767@gmail.com>
2023-08-14 17:49:14 +0000

    2-arguments.js

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,10 +1,8 @@
 #!/usr/bin/node
+const argc = process.argv.length;
 
-const numArgs = process.argv.length - 2;
-if (numArgs === 0) {
-  console.log('No argument');
-} else if (numArgs === 1) {
-  console.log('Arguments found');
+if (argc > 2) {
+  console.log('Argument' + (argc > 3 ? 's' : '') + ' found');
 } else {
-  console.log('Arguments found');
+  console.log('No argument');
 }
