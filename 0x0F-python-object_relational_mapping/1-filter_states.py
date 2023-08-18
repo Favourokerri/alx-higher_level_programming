35bb4375f53a1cd30646d2d7bb6c316636c30676
Favourokerri <favourokerri767@gmail.com>
2023-08-18 18:07:00 +0000

    made the select statement print the whole row

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -17,9 +17,9 @@ if __name__ == '__main__':
                                  passwd=argv[2], db=argv[3])
 
     db_cursor = db_connect.cursor()
-    db_cursor.execute("SELECT id, name FROM states WHERE name LIKE 'N%' \
+    db_cursor.execute("SELECT * FROM states WHERE name LIKE 'N%' \
                      ORDER BY states.id ASC")
-    names = db_cursor.fetchall()
+    states = db_cursor.fetchall()
 
-    for name in names:
-        print(name)
+    for state in states:
+        print(state)
