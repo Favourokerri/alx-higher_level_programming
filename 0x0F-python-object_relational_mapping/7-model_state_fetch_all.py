495b87e6d41983151529d8f86b8b6497d323e5db
Favourokerri <favourokerri767@gmail.com>
2023-08-19 21:01:43 +0000

    added a space

--- 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -21,6 +21,6 @@ if __name__ == '__main__':
     session = session()
 
     states = session.query(State).order_by(State.id).all()
-    [print('{}:{}'.format(state.id, state.name)) for state in states]
+    [print('{}: {}'.format(state.id, state.name)) for state in states]
 
     session.close()
