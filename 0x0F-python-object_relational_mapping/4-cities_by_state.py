3d77f3411b34c0620962e14b2cf1caf4bd995619
Favourokerri <favourokerri767@gmail.com>
2023-08-19 22:31:15 +0000

    4-cities_by_state.py

--- 0x0F-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -1,19 +1,28 @@
 #!/usr/bin/python3
-import MySQLdb as db
+
+"""
+This script lists all cities from the database in ascending order by id
+"""
+
+import MySQLdb
 from sys import argv
 
-"""
-    a script that lists all cities 
-    from the database hbtn_0e_4_usa
+if __name__ == '__main__':
 
-"""
-if __name__ =='__main__':
-    """
-        script takes 3 arguments:
-        mysql username, mysql password
-        and database name
+    user = argv[1]
+    password = argv[2]
+    database = argv[3]
 
-    """
-    db_connect = db.connect(host='localhost', user=argv[1], passwd=argv[2], port=3306, db=argv[3])
-    db_cursor = db_connect.cursor
+    db = MySQLdb.connect(host='localhost', port=3306, user=user,
+                         passwd=password, db=database)
 
+    cur = db.cursor()
+    cur.execute("""SELECT cities.id, cities.name, states.name
+    FROM cities INNER JOIN states ON cities.state_id = states.id
+    ORDER BY id ASC""")
+
+    rows = cur.fetchall()
+    for row in rows:
+        print(row)
+    cur.close()
+    db.close()
